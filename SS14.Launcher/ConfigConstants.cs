dfe43e62f9849ce478226f89c7c927f3e3b81a37
DEATHB4DEFEAT <zachcaffee@outlook.com>
2025-01-29 21:12:57 -0800

    move AuthServer class to a better spot

--- SS14.Launcher/ConfigConstants.cs
+++ SS14.Launcher/ConfigConstants.cs
@@ -32,52 +32,6 @@ public static class ConfigConstants
     // Amount of time to wait to let a redialling client properly die
     public const int LauncherCommandsRedialWaitTimeout = 1000;
 
-    // Yes, this is stupidly large, just collapse it if you don't want to see it
-    public class AuthServer(
-        Uri authUrl,
-        Uri accountSite,
-        bool? recommended = null,
-        string authAuthPath = "api/auth/authenticate",
-        string authRegPath = "api/auth/register",
-        string authPwResetPath = "api/auth/resetPassword",
-        string authResendPath = "api/auth/resendConfirmation",
-        string authPingPath = "api/auth/ping",
-        string authRefreshPath = "api/auth/refresh",
-        string authLogoutPath = "api/auth/logout",
-        string authAccountSitePath = "api/accountSite",
-        string accountManPath = "Identity/Account/Manage",
-        string accountRegPath = "Identity/Account/Register",
-        string accountResendPath = "Identity/Account/ResendEmailConfirmation")
-    {
-        public bool? Recommended { get; } = recommended;
-
-        public Uri AuthUrl { get; } = authUrl;
-        public string AuthAuthPath { get; } = authAuthPath;
-        public string AuthRegPath { get; } = authRegPath;
-        public string AuthPwResetPath { get; } = authPwResetPath;
-        public string AuthResendPath { get; } = authResendPath;
-        public string AuthPingPath { get; } = authPingPath;
-        public string AuthRefreshPath { get; } = authRefreshPath;
-        public string AuthLogoutPath { get; } = authLogoutPath;
-        public string AuthAccountSitePath { get; } = authAccountSitePath;
-        public string AuthAuthUrl { get; } = $"{authUrl}{authAuthPath}";
-        public string AuthRegUrl { get; } = $"{authUrl}{authRegPath}";
-        public string AuthPwResetUrl { get; } = $"{authUrl}{authPwResetPath}";
-        public string AuthResendUrl { get; } = $"{authUrl}{authResendPath}";
-        public string AuthPingUrl { get; } = $"{authUrl}{authPingPath}";
-        public string AuthRefreshUrl { get; } = $"{authUrl}{authRefreshPath}";
-        public string AuthLogoutUrl { get; } = $"{authUrl}{authLogoutPath}";
-        public string AuthAccountSiteUrl { get; } = $"{authUrl}{authAccountSitePath}";
-
-        public Uri AccountSite { get; } = accountSite;
-        public string AccountManPath { get; } = accountManPath;
-        public string AccountRegPath { get; } = accountRegPath;
-        public string AccountResendPath { get; } = accountResendPath;
-        public string AccountManUrl { get; } = $"{accountSite}{accountManPath}";
-        public string AccountRegUrl { get; } = $"{accountSite}{accountRegPath}";
-        public string AccountResendUrl { get; } = $"{accountSite}{accountResendPath}";
-    }
-
     public const string FallbackAuthServer = "Space-Wizards";
     public const string CustomAuthServer = "Custom";
     public static readonly AuthServer TemplateAuthServer = new(new("https://example.com/"), new("https://example.com/"));
@@ -159,4 +113,50 @@ public static class ConfigConstants
     public static readonly UrlFallbackSet UrlAssetsBase = LauncherDataBaseUrl + "assets/";
 
     public const string FallbackUsername = "JoeGenero";
+
+
+    public class AuthServer(
+        Uri authUrl,
+        Uri accountSite,
+        bool? recommended = null,
+        string authAuthPath = "api/auth/authenticate",
+        string authRegPath = "api/auth/register",
+        string authPwResetPath = "api/auth/resetPassword",
+        string authResendPath = "api/auth/resendConfirmation",
+        string authPingPath = "api/auth/ping",
+        string authRefreshPath = "api/auth/refresh",
+        string authLogoutPath = "api/auth/logout",
+        string authAccountSitePath = "api/accountSite",
+        string accountManPath = "Identity/Account/Manage",
+        string accountRegPath = "Identity/Account/Register",
+        string accountResendPath = "Identity/Account/ResendEmailConfirmation")
+    {
+        public bool? Recommended { get; } = recommended;
+
+        public Uri AuthUrl { get; } = authUrl;
+        public string AuthAuthPath { get; } = authAuthPath;
+        public string AuthRegPath { get; } = authRegPath;
+        public string AuthPwResetPath { get; } = authPwResetPath;
+        public string AuthResendPath { get; } = authResendPath;
+        public string AuthPingPath { get; } = authPingPath;
+        public string AuthRefreshPath { get; } = authRefreshPath;
+        public string AuthLogoutPath { get; } = authLogoutPath;
+        public string AuthAccountSitePath { get; } = authAccountSitePath;
+        public string AuthAuthUrl { get; } = $"{authUrl}{authAuthPath}";
+        public string AuthRegUrl { get; } = $"{authUrl}{authRegPath}";
+        public string AuthPwResetUrl { get; } = $"{authUrl}{authPwResetPath}";
+        public string AuthResendUrl { get; } = $"{authUrl}{authResendPath}";
+        public string AuthPingUrl { get; } = $"{authUrl}{authPingPath}";
+        public string AuthRefreshUrl { get; } = $"{authUrl}{authRefreshPath}";
+        public string AuthLogoutUrl { get; } = $"{authUrl}{authLogoutPath}";
+        public string AuthAccountSiteUrl { get; } = $"{authUrl}{authAccountSitePath}";
+
+        public Uri AccountSite { get; } = accountSite;
+        public string AccountManPath { get; } = accountManPath;
+        public string AccountRegPath { get; } = accountRegPath;
+        public string AccountResendPath { get; } = accountResendPath;
+        public string AccountManUrl { get; } = $"{accountSite}{accountManPath}";
+        public string AccountRegUrl { get; } = $"{accountSite}{accountRegPath}";
+        public string AccountResendUrl { get; } = $"{accountSite}{accountResendPath}";
+    }
 }
