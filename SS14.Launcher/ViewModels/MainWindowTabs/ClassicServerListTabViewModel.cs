d116bb4d9b228307cb917aef089fa8724710b410
VMSolidus <evilexecutive@gmail.com>
2026-01-16 14:01:05 -0500

    And now its done

--- SS14.Launcher/ViewModels/MainWindowTabs/ClassicServerListTabViewModel.cs
+++ SS14.Launcher/ViewModels/MainWindowTabs/ClassicServerListTabViewModel.cs
@@ -2,6 +2,7 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
 using Splat;
+using ReactiveUI;
 using SS14.Launcher.Models.ServerStatus;
 using SS14.Launcher.Localization;
 using SS14.Launcher.Utility;
@@ -15,6 +16,18 @@ public class ClassicServerListTabViewModel : MainWindowTabViewModel
     private readonly LocalizationManager _loc = LocalizationManager.Instance;
 
     public override string Name => _loc.GetString("tab-servers-classic-title"); 
+
+    private string? _searchString;
+
+    public string? SearchString
+    {
+        get => _searchString;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _searchString, value);
+            UpdateList();
+        }
+    }
 
     public ObservableCollection<ClassicServerEntryViewModel> AllServers { get; } = new();
 
@@ -36,13 +49,22 @@ public class ClassicServerListTabViewModel : MainWindowTabViewModel
     private void UpdateList()
     {
         AllServers.Clear();
-        // Sort by Players descending
-        var sorted = _cache.AllServers.OrderByDescending(s => s.PlayerCount).ToList();
+        // Filter then Sort by Players descending
+        var filtered = _cache.AllServers.Where(DoesSearchMatch);
+        var sorted = filtered.OrderByDescending(s => s.PlayerCount).ToList();
         
         foreach (var s in sorted)
         {
             AllServers.Add(new ClassicServerEntryViewModel(s));
         }
+    }
+
+    private bool DoesSearchMatch(ClassicServerStatusData data)
+    {
+        if (string.IsNullOrWhiteSpace(_searchString))
+            return true;
+        
+        return data.Name.Contains(_searchString, System.StringComparison.CurrentCultureIgnoreCase);
     }
 
     public override async void Selected()
